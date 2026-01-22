e9537807b3e2c3ca50f569b64140d7a912fb7299
frdge-dev <alvaro.makes.music@gmail.com>
2026-01-22 16:10:54 +0100

    Removed notes functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import { ThemeProvider } from "@/providers/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
 import Header from "@/components/Header";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import AppSidebar from "@/components/ui/AppSidebar";
-import NoteProvider from "@/providers/NoteProvider";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
@@ -28,19 +26,16 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <NoteProvider>
-            <SidebarProvider>
-              <AppSidebar/>
-              <div className="flex min-h-screen w-full flex-col">
-                <Header/>
-                <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
-                  {children}
-                </main>
-                <Footer/>
-              </div>
-              <Toaster/>
-            </SidebarProvider>
-          </NoteProvider>
+          <SidebarProvider>
+            <div className="flex min-h-screen w-full flex-col">
+              <Header />
+              <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
+                {children}
+              </main>
+              <Footer />
+            </div>
+            <Toaster />
+          </SidebarProvider>
         </ThemeProvider>
       </body>
     </html>
--- src/middleware.ts
+++ src/middleware.ts
@@ -45,48 +45,12 @@ export async function updateSession(request: NextRequest) {
     request.nextUrl.pathname === '/sign-up'
 
   // We don't wanna get the user everytime we update the session on the
-  // middleware. Only get it if
-  //   A) you're trying to log in or sign up afetr having logged in, or
-  //   B) you're going to home route and don't have any notes yet (no search
-  //      params)
+  // middleware. Redirect logged-in users away from auth pages
   if (isAuthRoute) {
     const { data: { user } } = await supabase.auth.getUser()
     if (user) {
       console.log('Trying to hit login or signup after having logged in; redirecting to home')
       return NextResponse.redirect(new URL('/', process.env.NEXT_PUBLIC_BASE_URL))
-    }
-  }
-
-  const { searchParams, pathname } = new URL(request.url)
-  if (!searchParams.get('noteId') && pathname === '/') {
-    const { data: { user } } = await supabase.auth.getUser()
-    // There's no known id in the search params, and we're on the home screen, and
-    // there's a user, we need to set a note id (if they have any node id, load
-    // the most recent one, else load a new note).
-    if (user) {
-      // You can't use prisma in middleware bc Next operates in an Edge
-      // environment which is different than a typical NodeJS environment.
-      const { newestNoteId } = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/fetch-newest-note?userId=${user.id}`)
-        .then(res => res.json())
-
-      if (newestNoteId) {
-        const url = request.nextUrl.clone()
-        url.searchParams.set('noteId', newestNoteId)
-        return NextResponse.redirect(url)
-      } else {
-        const { noteId } = await fetch(
-          `${process.env.NEXT_PUBLIC_BASE_URL}/api/create-new-note?userId=${user.id}`,
-          {
-            method: 'POST',
-            headers: {
-              "Content-Type": 'application/json'
-            }
-          }
-        ).then(res => res.json())
-        const url = request.nextUrl.clone()
-        url.searchParams.set('noteId', noteId)
-        return NextResponse.redirect(url)
-      }
     }
   }
   return supabaseResponse
